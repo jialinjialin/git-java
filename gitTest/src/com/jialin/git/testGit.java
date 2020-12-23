abfe844a0cd7fa734a247324219ac7b011c9e7df
jialin_glb <jialin_glb@qq.com>
2020-12-23 22:56:50 +0800

    将java工程添加到本地仓库

---
+++ gitTest/src/com/jialin/git/testGit.java
@@ -0,0 +1,17 @@
+package com.jialin.git;
+
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
+    }
+}
